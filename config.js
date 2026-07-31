2e1305af4018ae06c936bd450e8b438d535f5328
wckdgod <wckd_god@outlook.com>
2026-08-01 00:33:48 +0800

    Bump extension version to 2.0.0

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export const API_URL = "https://api.simkl.com";
 export const CLIENT_ID = "a63b63d85af0e02d4cfc791d87c881f710693ecc86d280fc98f8618f8f1faaad";
 export const CLIENT_SECRET = "9df1b7e078119eec54b694c216667be23f5a0c434aa7e7792e1ddbffb7d47336";
 export const APP_NAME = "crunchflix";
-export const APP_VERSION = "1.7.4";
+export const APP_VERSION = "2.0.0";
 
 export function getSimklUrl(endpoint) {
     const separator = endpoint.includes('?') ? '&' : '?';
